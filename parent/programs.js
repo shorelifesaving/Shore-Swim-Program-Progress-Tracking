092e08928387a2cac7327781f0435164c8be662d
shorelifesaving <lifesavingsquad@gmail.com>
2026-01-07 16:53:45 -0500

    Create programs.js

---
+++ parent/programs.js
@@ -0,0 +1,54 @@
+const programs = {
+  Starfish: {
+    "Starfish 1": [
+      "Back float (5 sec, assisted)",
+      "Front float (5 sec, assisted)",
+      "Roll over float (5 sec, assisted)",
+      "Back glide (5 sec, assisted)",
+      "Front glide (5 sec, assisted)"
+    ],
+    "Starfish 2": [
+      "Back float (5 sec)",
+      "Front float (5 sec)",
+      "Roll over float (5 sec, assisted)",
+      "Back glide (5 sec, assisted)",
+      "Front glide (5 sec, assisted)",
+      "Kicking on front (2 m, assisted)",
+      "Kicking on back (2 m, assisted)",
+      "Jump in pool, swim to wall, climb out (assisted)"
+    ],
+    "Starfish 3": [
+      "Roll over float (5 sec)",
+      "Back glide (5 sec)",
+      "Front glide (5 sec)",
+      "Kicking on front (2 m, assisted)",
+      "Kicking on back (2 m, assisted)",
+      "Roll over glide (10 m, assisted)",
+      "Jump in pool, swim to wall, climb out (assisted)"
+    ],
+    "Starfish 4": [
+      "Kicking on front (2 m)",
+      "Kicking on back (2 m)",
+      "Roll over glide (15 m)",
+      "Jump in pool, swim to wall, climb out"
+    ],
+    "Starfish 5": [
+      "Roll over glide (15 m)",
+      "Kicking on front with 4 front crawl arms",
+      "Kicking on side (5 m, assisted)",
+      "Jump in pool, tread water, climb out (15 sec, assisted)"
+    ],
+    "Starfish 6": [
+      "Kicking on side (5 m)",
+      "4 front crawl arms, roll to side, with flutterboard (10 m, assisted)",
+      "Back crawl with 6 kicks, 1 arm (10 m, assisted)",
+      "Jump in pool, tread water, climb out (15 sec)"
+    ],
+    "Starfish 7": [
+      "Kicking on side (10 m)",
+      "4 front crawl arms, roll to side, with flutterboard (15 m)",
+      "Back crawl with 6 kicks, 1 arm (15 m)",
+      "Jump in pool, tread water, climb out (30 sec)"
+    ]
+  }
+};
