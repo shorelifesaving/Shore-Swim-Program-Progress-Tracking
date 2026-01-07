f8e7d9598997154e77a075c02b7c4882e19edc13
shorelifesaving <lifesavingsquad@gmail.com>
2026-01-07 16:55:42 -0500

    Update programs.js

--- parent/programs.js
+++ parent/programs.js
@@ -50,5 +50,96 @@ const programs = {
       "Back crawl with 6 kicks, 1 arm (15 m)",
       "Jump in pool, tread water, climb out (30 sec)"
     ]
+  },
+
+  "Sea Turtle": {
+    "Sea Turtle 1": [
+      "Back float (5 sec)",
+      "Front float (5 sec)",
+      "Roll over float (5 sec)",
+      "Back glide (5 sec)",
+      "Front glide (5 sec)",
+      "Kicking on front (2 m, assisted)",
+      "Kicking on back (2 m, assisted)",
+      "Jump in pool, swim to wall, climb out (assisted)"
+    ],
+    "Sea Turtle 2": [
+      "Roll over glide (15 m)",
+      "Kicking on front with 4 front crawl arms",
+      "Kicking on side (5 m, assisted)",
+      "Jump in pool, swim to wall, climb out"
+    ],
+    "Sea Turtle 3": [
+      "Kicking on side (10 m)",
+      "4 front crawl arms, roll to side, with flutterboard (15 m)",
+      "Back crawl with 6 kicks, 1 arm (15 m)",
+      "Jump in pool, tread water, climb out (30 sec)"
+    ],
+    "Sea Turtle 4": [
+      "4 front crawl arms, roll to side (25 m)",
+      "Back crawl with 3 kicks, 1 arm (25 m)",
+      "Treading water (60 sec)"
+    ],
+    "Sea Turtle 5": [
+      "2 front crawl arms, roll to side, with flutterboard (50 m)",
+      "Back crawl with continuous arms (50 m)",
+      "Treading water (90 sec)"
+    ],
+    "Sea Turtle 6": [
+      "2 front crawl arms, roll to side (50 m)",
+      "Back crawl (50 m)",
+      "Whip kick on back (25 m)",
+      "Treading water (120 sec)"
+    ],
+    "Sea Turtle 7": [
+      "Front crawl with bilateral breathing (75 m)",
+      "Back crawl (75 m)",
+      "Whip kick on front (25 m)",
+      "Eggbeater (30 sec)"
+    ],
+    "Sea Turtle 8": [
+      "Front crawl with bilateral breathing (75 m)",
+      "Back crawl with bent arm pull (75 m)",
+      "Breaststroke (25 m, assisted)",
+      "Eggbeater (60 sec)"
+    ],
+    "Sea Turtle 9": [
+      "Front crawl with bent arms and bilateral breathing (75 m)",
+      "Back crawl with bent arm pull (75 m)",
+      "Breaststroke (25 m)",
+      "Eggbeater (120 sec)",
+      "Kneeling dive"
+    ],
+    "Sea Turtle 10": [
+      "Front crawl with bent arms and bilateral breathing (100 m)",
+      "Back crawl with bent arm pull (100 m)",
+      "Breaststroke (50 m)",
+      "Swim 300 m continuously",
+      "Eggbeater (120 sec)",
+      "Standing dive"
+    ]
+  },
+
+  Sunfish: {
+    "Sunfish 11": [
+      "Swim 400 m continuously (13 min)",
+      "Head up front crawl (25 m)",
+      "Head up breaststroke (25 m)",
+      "Whip kick on back (25 m)",
+      "Eggbeater kick on back (25 m)",
+      "Foot first surface dive, underwater swim (5 m), and surface",
+      "Eggbeater (150 sec)",
+      "Stride entry with noodle"
+    ],
+    "Sunfish 12": [
+      "Swim 400 m continuously (12 min)",
+      "Head up front crawl (50 m)",
+      "Head up breaststroke (50 m)",
+      "Towing (25 m)",
+      "Head first surface dive, underwater swim (5 m), and surface",
+      "Eggbeater (180 sec)",
+      "Stride entry"
+    ]
   }
 };
+
